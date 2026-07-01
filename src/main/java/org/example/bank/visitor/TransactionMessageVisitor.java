3edf2c19e809c961664da240a1fa496989fb65e9
nikmakeev294-sudo <nikmakeev294@gmail.com>
2026-07-01 16:14:17 +0300

    Fix locale-independent transaction messages

--- src/main/java/org/example/bank/visitor/TransactionMessageVisitor.java
+++ src/main/java/org/example/bank/visitor/TransactionMessageVisitor.java
@@ -2,12 +2,15 @@ package org.example.bank.visitor;
 
 import org.example.bank.model.Transaction;
 
+import java.util.Locale;
+
 public class TransactionMessageVisitor implements TransactionVisitor {
     private String message;
 
     @Override
     public void visitDeposit(Transaction transaction) {
         message = String.format(
+                Locale.US,
                 "Deposit operation: account %s received %.2f",
                 transaction.getToAccountNumber(),
                 transaction.getAmount()
@@ -17,6 +20,7 @@ public class TransactionMessageVisitor implements TransactionVisitor {
     @Override
     public void visitWithdraw(Transaction transaction) {
         message = String.format(
+                Locale.US,
                 "Withdraw operation: account %s spent %.2f",
                 transaction.getFromAccountNumber(),
                 transaction.getAmount()
@@ -26,6 +30,7 @@ public class TransactionMessageVisitor implements TransactionVisitor {
     @Override
     public void visitTransfer(Transaction transaction) {
         message = String.format(
+                Locale.US,
                 "Transfer operation: %.2f from %s to %s",
                 transaction.getAmount(),
                 transaction.getFromAccountNumber(),
